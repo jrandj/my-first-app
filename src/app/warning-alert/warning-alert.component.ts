a1153995f1b73995ed8d19f81d3aac8ecd6967a6
jrandj <james.randjelovic@gmail.com>
2021-01-09 20:29:57 +1100

    Update with solution to Assignment 2: Practicing Databinding

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-warning-alert',
     templateUrl: './warning-alert.component.html',
-    //styleUrls: ['/warning-alert.component.css']
     styles: [
         `
         p {
