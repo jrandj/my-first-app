d61a42847de06f6cf18d11555b1dbd74b43c0686
jrandj <james.randjelovic@gmail.com>
2021-01-18 22:00:46 +1100

    Revert "Update with solution to Assignment 4: Practicing Property & Event Binding and View Encapsulation"
    This reverts commit 8ca16c4dfe6ef6d1ed74038e063ab686af20c04c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
